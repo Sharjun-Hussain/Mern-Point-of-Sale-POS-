5cf863860ade839b408bc5902a34681e48d838de
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-08 13:34:45 +0530

    Some Edit in Backend

--- Back-End/Server.js
+++ Back-End/Server.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const cors= require('cors')
-const mongoose = require('mongoose')
-
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get('/', (req,res)=>{
-    res.status(200).json({
-        "message":"Hello"
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello",
+  });
+});
 
-app.listen(8000, ()=>{
-    console.log("Server Is listening ");
-})
+app.listen(8000, () => {
+  console.log("Server Is listening ");
+});
