90939f756d428308797ef3b9f26872451b7d9013
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-10 16:48:01 +0530

    Added Register User and Login User Validation Perfectly Workds fine

--- BackEnd/Controllers/UserController.js
+++ BackEnd/Controllers/UserController.js
@@ -4,39 +4,73 @@ const bcrypt = require("bcryptjs");
 
 exports.RegisterUser = async (req, res, next) => {
   const userDataFromReguest = req.body;
-  const { username, email } = req.body;
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ Message: "Please Completly Fill THe Input Fields" });
+  }
 
   const ExistingUser = await usersModel.findOne({
-   $or: [{username},{email}]
+    $or: [{ username }, { email }],
   });
 
   if (!ExistingUser) {
     const user = await usersModel.create(userDataFromReguest);
+    const generatedToken = user.generatejwtToken();
+
     if (user) {
       res.status(200).json({
-        "Success ": true,
-        user,
-        username,
-        email,
+        Message: " Your Account Creation SuccessFull! ",
+        generatedToken,
       });
     }
   }
 
   if (ExistingUser) {
     return res.status(400).json({
-      success: "false",
       Message: " User Is Already Found Please Sign In",
     });
   }
 };
 
 exports.LoginUser = async (req, res, next) => {
-  const username = req.body;
-  const password = req.body.password;
+  const { username, email, password } = req.body;
 
-  res.status(200).json({
-    success: "true",
-    username,
-    password,
-  });
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ Message: "Please Completly Fill THe Input Fields" });
+  }
+
+  try {
+    const user = await usersModel.findOne({email});
+    if (!user) {
+      return res.status(400).json({
+        Message: "User OR Password Incorrect",
+      });
+    }
+
+    if (user) {
+      try {
+        if (!(await user.isValidPassword(password))) {
+          return res.status(400).json({
+            Message: "Invalid UserName OR Password",
+          });
+        }
+
+        const generatedToken = user.generatejwtToken();
+        res.status(200).json({
+          Message: "Login SuccessFull",
+          generatedToken,
+          user,
+        });
+      } catch (err) {}
+    }
+  } catch (err) {
+    return res.status(500).json({
+      Message: "Internal server Error",
+    });
+  }
 };
--- BackEnd/Models/UserModel.js
+++ BackEnd/Models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 
 const userSchema = new mongoose.Schema({
@@ -12,16 +13,28 @@ const userSchema = new mongoose.Schema({
         type: String,
         unique: true,
         required: true,
+        
       },
       password: {
         type: String,
         required: true,
+        Select : false
       },
 })
 
 userSchema.pre('save', async function(next){
   this.password = await bcrypt.hash(this.password,10);  
 })
+
+userSchema.methods.generatejwtToken = function(){
+  return jwt.sign({id:this.id},process.env.JWT_SECRET,{
+    expiresIn:process.env.JWT_SECRET_EXPIRES
+  })
+}
+
+userSchema.methods.isValidPassword = async function(Enteredpassword){
+ return await bcrypt.compare(Enteredpassword,this.password);
+}
 
 const users = mongoose.model('user', userSchema);
 module.exports = users;
