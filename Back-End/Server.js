feb7305a55cd3b462edc34f5d4b25bfedb775f28
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-10 13:28:59 +0530

    dotenv Changes And Enhancement

--- Back-End/Server.js
+++ Back-End/Server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv')
 
+dotenv.config({path:'.env'})
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Hello",
-  });
-});
 
-app.listen(8000, () => {
-  console.log("Server Is listening ");
+app.get('/api', )
+
+app.listen(process.env.PORT, (con) => {
+  console.log(`Server Is listening  : ${process.env.PORT} ` );
 });
