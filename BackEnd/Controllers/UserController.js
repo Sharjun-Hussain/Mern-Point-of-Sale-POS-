99212ad22975d4897f35157c2f3a6694df25c0d5
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-10 15:53:21 +0530

    Added Register User Validation

--- BackEnd/Controllers/UserController.js
+++ BackEnd/Controllers/UserController.js
@@ -4,23 +4,39 @@ const bcrypt = require("bcryptjs");
 
 exports.RegisterUser = async (req, res, next) => {
   const userDataFromReguest = req.body;
-  const user = await usersModel.create(userDataFromReguest);
-  if(user) {
-    res.status(200).json({
+  const { username, email } = req.body;
+
+  const ExistingUser = await usersModel.findOne({
+   $or: [{username},{email}]
+  });
+
+  if (!ExistingUser) {
+    const user = await usersModel.create(userDataFromReguest);
+    if (user) {
+      res.status(200).json({
         "Success ": true,
-        user
-    })
+        user,
+        username,
+        email,
+      });
+    }
+  }
+
+  if (ExistingUser) {
+    return res.status(400).json({
+      success: "false",
+      Message: " User Is Already Found Please Sign In",
+    });
   }
 };
 
+exports.LoginUser = async (req, res, next) => {
+  const username = req.body;
+  const password = req.body.password;
 
-exports.LoginUser = async (req,res,next) =>{
-    const username = req.body;
-    const password = req.body.password;
-
-    res.status(200).json({
-        success:"true",
-        username ,
-        password
-    })
-}
+  res.status(200).json({
+    success: "true",
+    username,
+    password,
+  });
+};
