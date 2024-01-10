3f3214e8bc569b5df319e2ce81690a2214b9229b
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-10 15:35:20 +0530

    Password Hashing Added

--- BackEnd/Models/UserModel.js
+++ BackEnd/Models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 
 const userSchema = new mongoose.Schema({
@@ -16,6 +17,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
+})
+
+userSchema.pre('save', async function(next){
+  this.password = await bcrypt.hash(this.password,10);  
 })
 
 const users = mongoose.model('user', userSchema);
